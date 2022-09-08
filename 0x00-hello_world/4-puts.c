f8877c081416e36ceb63823829f9fbf77876216d
Joseph Wainaina <ynainajoseph@gmail.com>
2022-09-08 05:10:44 -0700

    puts;

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
-/*
- * Write a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 
 int main(void) 
