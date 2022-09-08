b210b7c8f6216589ab1c9d79a76a7f35ac66b6da
Joseph Wainaina <ynainajoseph@gmail.com>
2022-09-08 07:44:27 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * return: always 1 (success)
+ */
+int main(void)
+{
+	write(STDOUT_FILENO, "and that piece of art is useful\" 
+			- Dora Korpar, 2015-10-19");
+	return (1);
+}
