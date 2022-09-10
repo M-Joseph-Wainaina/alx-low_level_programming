aeb24fd45331164d75fc92a24da33563ac2f976d
Joseph Wainaina <ynainajoseph@gmail.com>
2022-09-10 08:06:49 -0700

    combo5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,17 +13,16 @@ int main(void)
 	{
 		for (b = a + 1; b < 100; b++)
 		{
-					putchar((a / 10) + '0');
-					putchar((a % 10) + '0');
-					putchar(' ');
-					putchar((b / 10) + '0');
-					putchar((b % 10) + '0');
-
-					if (a == 98 && b == 99)
-						continue;
-					putchar(',');
-					putchar(' ');
-				}
+			putchar((a / 10) + '0');
+			putchar((a % 10) + '0');
+			putchar(' ');
+			putchar((b / 10) + '0');
+			putchar((b % 10) + '0');
+			if (a == 98 && b == 99)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
