d36fbfc602485ac0fb64fdce2dd49948877a6651
Joseph Wainaina <ynainajoseph@gmail.com>
2022-09-13 07:24:35 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char c;
+
+	for(c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+	_putchar('\n');
+	return (0);
+}
