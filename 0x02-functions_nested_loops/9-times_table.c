75d8169e292b40e7ed9c83d875723ce9dd348084
Joseph Wainaina <ynainajoseph@gmail.com>
2022-09-14 06:31:48 -0700

    space before

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,20 +16,20 @@ void times_table(void)
 		{
 			if ((r * c) > 9)
 			{
-			_putchar(((r * c) / 10) + '0');
+			_putchar(' ');
+			_putchar((( * c) / 10) + '0');
 			_putchar(((r * c) % 10) + '0');
 			if (c == 9)
 				continue;
 			_putchar(',');
-			_putchar(' ');
 			}
 			else
 			{
+			_putchar(' ');
 			_putchar(((r * c) % 10) + '0');
 			if (c == 9)
 				continue;
 			_putchar(',');
-			_putchar(' ');
 			_putchar(' ');
 			}
 		}
