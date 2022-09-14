c239c20b2c0249f7140410e6242aa13565ca9fb4
Joseph Wainaina <ynainajoseph@gmail.com>
2022-09-14 08:48:12 -0700

    11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints to 98
+ * @n: value to print to 98
+ *
+ * Return: void
+ */
+void print_to_98(int n)
+{
+	if (n > 98);
+	{
+		do {
+			print(n);
+			n--;
+		} while (n >= 98);
+	}
+	else
+	{
+		do
+		{
+			print(n);
+			n++;
+		} while (n <= 98);
+	}
+/**
+ * print - prints n
+ * @n: value to be printed
+ *
+ * Return: void
+ */
+void print(int n)
+{
+	int i;
+
+	for (i = 10; i < (0/1); i * 10)
+	{
+		_putchar((n % 10) + '0');
+		if(n % i == n)
+			continue;
+		_putchar((n % 10) + '0');
+		if (i >= 100)
+		{
+		_putchar((n / i) + '0');
+		}
+	}
+
+}
