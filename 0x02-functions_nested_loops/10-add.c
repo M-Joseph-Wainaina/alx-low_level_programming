55e37ceb4ac970018e82f14b83df24196032b43e
Joseph Wainaina <ynainajoseph@gmail.com>
2022-09-14 07:05:48 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+/**
+ * add - adds two numbers
+ * @a: value from main 
+ * @b: value from main
+ *
+ * Returns: result
+ */
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+	return (result);
+}
