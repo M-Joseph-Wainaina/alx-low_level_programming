4bf387ef91624c0deb836a4af3fdd225ad228786
Joseph Wainaina <ynainajoseph@gmail.com>
2022-09-14 10:29:39 -0700

    yea

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,36 +11,38 @@ void print_to_98(int n)
 	if (n > 98)
 	{
 		do {
-			pront(n);
+			int i;
+
+        for (i = 10; i < (0 / 1);i = i * 10)
+        {
+                _putchar((n % 10) + '0');
+                if (n % i == n)
+                        continue;
+                if (i >= 100)
+                {
+                _putchar((n / i) + '0');
+                }
+        }
+       
 			n--;
 		} while (n >= 98);
 	}
 	else
 	{
 		do {
-			pront(n);
+			int i;
+
+        for (i = 10; i < (0 / 1);i = i * 10)
+        {
+                _putchar((n % 10) + '0');
+                if (n % i == n)
+                        continue;
+                if (i >= 100)
+                {
+                _putchar((n / i) + '0');
+                }
+        }
+       
 			n++;
 		} while (n <= 98);
-	}
-/**
- * print - prints n
- * @n: value to be printed
- *
- * Return: always 0
- */
-int pront(int n)
-{
-	int i;
-
-	for (i = 10; i < (0 / 1);i = i * 10)
-	{
-		_putchar((n % 10) + '0');
-		if (n % i == n)
-			continue;
-		if (i >= 100)
-		{
-		_putchar((n / i) + '0');
-		}
-	}
-	return (0)
 }
