84ac5f4c3ffe5f352b2495f2e167496b98b9d59f
Joseph Wainaina <ynainajoseph@gmail.com>
2022-09-15 14:02:27 -0700

    prime_number

---
+++ 0x04-more_functions_nested_loops/100-prime_number.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - prints prime number
+ * Return: 0
+ */
+
+int main()
+{
+	long long int p, n;
+
+	n = 612852475143;
+
+	for (p = 2; p <= n; p++)
+	{
+		if (n % p == 0)
+		{
+			do {
+				n = n / p;
+			} while (n % p == 0);
+
+		}
+
+	}
+
+	printf("%lld\n", p);
+	return (0);
+}
