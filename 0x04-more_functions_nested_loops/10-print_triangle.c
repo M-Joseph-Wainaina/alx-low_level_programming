ec3122398f0f1ca90a7a717e3845d1fde563d131
Joseph Wainaina <ynainajoseph@gmail.com>
2022-09-15 10:35:56 -0700

    tringles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints triangle
+ * @size: size of triangle passed from main
+ *
+ * Return: void
+ */
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	if (size > 0)
+	{
+		for (j = 0; j <= size; j++)
+		{
+		for (i = size - j; i > 0; i--)
+		{
+			_putchar(' ');
+		}
+		for (k = j; k > 0; k--)
+		{
+		_putchar('#');
+		}
+		if (j == size)
+			continue;
+		_putchar('\n');
+		}
+	}
+	else if(size <= 0)
+	{
+		_putchar('\n');
+	}
+}
