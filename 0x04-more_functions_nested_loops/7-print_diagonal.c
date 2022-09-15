6002b919c00a4575ddfd9164a607aabaf836042d
Joseph Wainaina <ynainajoseph@gmail.com>
2022-09-15 07:46:22 -0700

    mutoi

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,11 +10,12 @@ void print_diagonal(int n)
 {
 	int r;
 	int i;
+
 	if (n > 0)
 	{
 	for (i = 0; i < n; i++)
 	{
-		for (r=0; r <= i; r++)
+		for (r=0; r < i; r++)
 		{
 		_putchar(' ');
 			continue;
