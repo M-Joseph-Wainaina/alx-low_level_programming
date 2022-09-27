fa1d8e656680b9177f88876da9343d9c2fb03f2b
joseph wainaina <josephynaina@gmail.com>
2022-09-27 20:48:42 +0000

    chess

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints the chessboard
+ * @a: pointer to the first character
+ * Return: void
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int b, c; 
+
+	b = 0;
+	c = 0;
+
+	while (b < 8)
+	{
+		while (c < 8)
+		{
+			_putchar(a[b][c]);
+			c++;
+		}
+		c = 0;
+		b++;
+		_putchar("\n");
+
+	}
+}
