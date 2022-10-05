41c76324f2a221c5a5e726ad48685727432d4319
joseph wainaina <josephynaina@gmail.com>
2022-10-05 13:41:42 +0000

    betty style of coding

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,16 +1,23 @@
 #include "main.h"
+int check(char **av, int a, int ac);
 
 /**
  * argstostr - converts command line
  * arguments to a string
- * @ac - number of arguments
- * @av - arguments
+ * @ac: number of arguments
+ * @av: ariguments
+ * Return: void pointer to char
  */
 
 char *argstostr(int ac, char **av)
 {
 	int a, b, c, len;
 	char *p;
+
+	if (ac == 0 || check(av, 1, ac) == 1)
+	{
+		return (NULL);
+	}
 
 	len = 0;
 	for (a = 0; a < ac; a++)
@@ -22,7 +29,7 @@ char *argstostr(int ac, char **av)
 			len++;
 		}
 	}
-	p = malloc((sizeof(char) * len) + ac);
+	p = malloc((sizeof(char) * len) + ac + 1);
 	if (p == NULL)
 	{
 		free(p);
@@ -43,7 +50,26 @@ char *argstostr(int ac, char **av)
 	return (p);
 }
 
-		       	
+/**
+ * check - check for null in arguments
+ * @av: arguments
+ * @ac: number of arguments
+ * @a: iterator
+ * Return: 1 if NULL found or 0 otherwise
+ */
+
+int check(char **av, int a, int ac)
+{
+	if (av[a] == NULL)
+	{
+		return (1);
+	}
+	if (a == ac - 1)
+	{
+		return (0);
+	}
+	return (check(av, a + 1, ac));
+}
 
 
 
