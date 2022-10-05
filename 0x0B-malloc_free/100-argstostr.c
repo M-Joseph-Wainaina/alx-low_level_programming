512d5ce845ea2a778b1bf21e77433acd3c7c58b3
joseph wainaina <josephynaina@gmail.com>
2022-10-05 13:02:45 +0000

    100

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * argstostr - converts command line
+ * arguments to a string
+ * @ac - number of arguments
+ * @av - arguments
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int a, b, c, len;
+	char *p;
+
+	if (
+	len = 0;
+	for (a = 0; a < ac; a++)
+	{
+		b = 0;
+		while (av[a][b])
+		{
+			b++;
+			len++;
+		}
+	}
+	p = malloc((sizeof(char) * len) + ac);
+	if (p == NULL)
+	{
+		free(p);
+		return (NULL);
+	}
+	c = 0;
+	for (a = 0; a < ac; a++)
+	{
+		b = 0;
+		while (av[a][b])
+		{
+			p[c] = av[a][b];
+			b++;
+			c++;
+		}
+		p[c++] = '\n';
+	}
+	return (p);
+}
+
+		       	
+
+
+
