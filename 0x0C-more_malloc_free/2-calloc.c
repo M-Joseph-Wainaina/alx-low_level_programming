5c373296809eb7de6bdf5de94a37d453c375b073
joseph wainaina <josephynaina@gmail.com>
2022-10-06 10:35:11 +0000

    _calloc function created

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _calloc: allocates memmory for an array
+ * @nmemb: number of elements
+ * @size: size in bytes
+ * Return: void pointer
+ */
+ 
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *p;
+	unsigned int a;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	p = malloc(nmemb * size);
+	if (p == NULL)
+		return (NULL);
+	for (a = 0; a < nmemb; a++)
+	{
+		p[a] = 0;
+	}
+	return (p);
+}
+
