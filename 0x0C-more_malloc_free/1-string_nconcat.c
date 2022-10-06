0f4305f3dfcd0e01f0e062d1b23f4fe2d19b6261
joseph wainaina <josephynaina@gmail.com>
2022-10-06 09:03:41 +0000

    error in variable names fixed

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,7 +16,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	len1 = 0;
 	while(s1[len1])
 	{
-		len++;
+		len1++;
 	}
 	len2 = 0;
 	while (s2[len2])
@@ -27,7 +27,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		n = len2;
 	}
-	p = malloc(sizeof(char) * len);
+	p = malloc(sizeof(char) * len1);
 	for (a = 0; a < len1; a++)
 	{
 		p[a] = s1[a];
@@ -39,7 +39,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 	for (a = 0; a < n; a++)
 	{
-		p[len + a] = s2[a];
+		p[len1 + a] = s2[a];
 	}
 	
 	return (p);
