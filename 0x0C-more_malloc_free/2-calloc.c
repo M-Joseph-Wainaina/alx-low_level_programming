53750d678a4002e0ff6ed5f6a07ffaed71c4cae5
joseph wainaina <josephynaina@gmail.com>
2022-10-06 11:20:59 +0000

    calloc created

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,20 +9,18 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	int i = 0, l = 0;
 	char *p;
-	unsigned int a;
-	int l;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
+
 	l = nmemb * size;
 	p = malloc(l);
 	if (p == NULL)
 		return (NULL);
-	for (a = 0; a < nmemb; a++)
-	{
-		p[a] = 0;
-	}
+	while (i < l)									{
+		p[i] = 0;
+		i++;									}
 	return (p);
 }
-
