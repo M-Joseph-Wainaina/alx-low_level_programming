ce75711bed8a300db13b9b892daa2f32f6c293e9
joseph wainaina <josephynaina@gmail.com>
2022-10-11 14:01:46 +0000

    betty documentation style added

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,12 @@
 #ifndef _DOG_H_
 #define _DOG_H_
+/**
+ * struct dog - struct for name age and owner
+ * @name: dogs name
+ * @age: dogs age
+ * @owner: dogs owner
+ */
+
 struct dog
 {
 	char *name;
