1e9f6f35a1906110e5381500141d0668f181de6b
joseph wainaina <josephynaina@gmail.com>
2022-10-11 14:09:11 +0000

    function to intialise dogs details created

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+ * init_dog - initialises a dogs detail
+ * @d: pointer to new dogs details 
+ * @name: dogs name
+ * @age: dogs age
+ * @owner: dogs owner
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d.name = name;
+	d.age = age;
+	d.owner = owner;
+}
