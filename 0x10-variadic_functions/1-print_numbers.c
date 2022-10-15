e82728f34d79e4756a53240a3d2b50ba0e5e1ec9
joseph wainaina <josephynaina@gmail.com>
2022-10-15 12:18:43 +0000

    function that prints numbers created

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - function that prints numbers passed to itj
+ * @separator: char used to separate the list
+ * @n: number of integers passed
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+
+	va_start(args, n);
+	i = 0;
+
+	while (i < n)
+	{
+		printf("%d", va_arg(args, int));
+		if (i == (n - 1))
+			break;
+		printf("%c ", *separator);
+		i++;	
+	}
+
+	printf("\n");
+	va_end(args);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <stdarg.h>
 int sum_them_all(const unsigned int n, ...);
-
+void print_numbers(const char *separator, const unsigned int n, ...);
 
 
 #endif /*_header*/
