987799a3492b8ef9ba6e1fa3baaf120d76b8b040
joseph wainaina <josephynaina@gmail.com>
2022-11-03 17:37:46 +0000

    tasks completed

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,34 +1,31 @@
 #include "main.h"
 
-
 /**
- * read_textfile - function that reads a textfile
- * and prints it to the standard output
- * @letters: number of letters to be printed
- * @filename: file to be opened
- * Return: the actual number of letters it could read and print
- */
-
+  * read_textfile - ...
+  * @filename: The source file
+  * @letters: Number of letters to reads and prints
+  *
+  * Return: ...
+  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, r;
-	char *buff;
-
-	buff = malloc(sizeof(char) * letters);
+	int fd, readed;
+	char *buff = malloc(sizeof(char *) * letters);
 
 	if (!buff)
 		return (0);
+
 	if (!filename)
 		return (0);
 
-	fd = open(filename, O_RDONLY);
+	fd = open(filename, O_RDONLY, 0600);
 	if (fd == -1)
 		return (0);
 
-	r = read(fd, buff, letters);
-	write(STDOUT_FILENO, buff, r);
+	readed = read(fd, buff, letters);
+	write(STDOUT_FILENO, buff, readed);
 
-	close(fd);
 	free(buff);
-	return (r);
+	close(fd);
+	return (readed);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,18 +1,17 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef _FILE_IO_
+#define _FILE_IO_
 
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/uio.h>
 #include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
 
-#define BUFFSIZE 1024
-int _putchar(char);
 ssize_t read_textfile(const char *filename, size_t letters);
+int _strlen(char *s);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void copy_file(const char *src, const char *dest);
 
-
-#endif /*main.h*/
+#endif
