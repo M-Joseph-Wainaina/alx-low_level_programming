579f82621a6cb3db7f6bc5e39970154adb71b79f
joseph wainaina <josephynaina@gmail.com>
2022-12-09 16:28:49 +0000

    function that returns the length of a linked list

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - function that returns the number of elements in a linked list
+ * @h: head of the linked list
+ * Return: list len
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t i;
+	const dlistint_t *temp;
+
+	temp = h;
+	i = 0;
+
+	while(temp)
+	{
+		i++;
+		temp = temp->next;
+	}
+	return (i);
+}
