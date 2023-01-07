751e638936c4313b4614e9b24d5673bbe3ac7e84
joseph wainaina <josephynaina@gmail.com>
2023-01-07 16:35:03 +0000

    betty coding and documentation style used

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -2,34 +2,34 @@
 /**
  * hash_table_create - function that creates a hash table
  * @size: size of the hash table
- * @return: newly created hash table
+ * Return: newly created hash table
  */
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-/*	unsigned long int i;*/
+	unsigned long int i;
 
 	hash_table_t *table;
 
 	table = malloc(sizeof(hash_table_t));
-	
+
 	if (table == NULL)
 	{
 		return (NULL);
 	}
 
 	table->size = size;
-/*	table->array = malloc(sizeof(hash_node_t *) * size);
-	
+	table->array = malloc(sizeof(hash_node_t *) * size);
+
 	if (table->array == NULL)
 	{
 		return (NULL);
 	}
-	for(i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		table->array[i] = NULL;
 	}
-*/
+
 	return (table);
 
-}	
+}
